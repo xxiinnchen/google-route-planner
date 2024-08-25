092865ccf7133862db90b95fd74f11b303089ab5
Xin Chen <xinchen5@lbl.gov>
2024-08-24 19:35:12 -0700

    add github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  }
+  
+  module.exports = nextConfig
 
-export default nextConfig;
+  export default nextConfig;
+  
