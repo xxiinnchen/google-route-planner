41994713304856cbb0b170d038def55a16e1d3b0
Xin Chen <xinchen5@lbl.gov>
2024-08-24 19:56:46 -0700

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/google-route-planner' : '',
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  output: 'export',
+  basePath: '/google-route-planner',
+  assetPrefix: '/google-route-planner/',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
