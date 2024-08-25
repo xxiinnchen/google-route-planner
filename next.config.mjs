2c2d42836210564d93ffafce13eb85031f1e6c0b
Xin Chen <xinchen5@lbl.gov>
2024-08-24 19:49:16 -0700

    fix deploy typo

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/google-route-planner' : '',
     images: {
       unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/google-route-planner' : '',
   }
   
   module.exports = nextConfig
-  
-
   
